64a9b51fc06a5a2c03341fe9b6587f8c9e9a184e
orsiak <malecmichal2004@gmail.com>
2025-10-24 21:36:43 +0200

    komentarze wstepv1

--- blog123/models.py
+++ blog123/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Comment(models.Model):
     content = models.TextField()
     author = models.CharField(max_length=200)
-    #mozna dodac ze komentarze nie moga byc czesciej niz x czasu!!
+    #mozna dodac ze komentarze nie moga byc czesciej niz x czasu!!!
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f"Komentarz {self.author}"
