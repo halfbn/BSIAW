fb1378f39dbf5b3da1d8942a407739da930bafae
Wiktor Łangowski <wiktor.langowski@gmail.com>
2025-11-09 16:49:38 +0100

    Update ALLOWED_HOSTS for security configuration

--- bsiaw/settings.py
+++ bsiaw/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-)w!@3h3kxv18+atpf%*r85n(b5^8*45^)sihr(^8e=82!l_*t&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['10.0.0.0/16']
 
 # Application definition
 
