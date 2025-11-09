1d05a9b6e2ba37aeacb53846bc0f21600a8d5bb9
Wiktor Łangowski <wiktor.langowski@gmail.com>
2025-11-09 17:30:02 +0100

    Restrict ALLOWED_HOSTS to only '*'

--- bsiaw/settings.py
+++ bsiaw/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)w!@3h3kxv18+atpf%*r85n(b5^8*45^)sihr(^8e=82!l_*t&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
