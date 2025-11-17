3a8b0cd43d502dec3d134f57faffa4780da6b53e
halfbn <279460@student.pwr.edu.pl>
2025-11-17 19:43:42 +0100

    Change ALLOWED_HOSTS to allow all hosts

--- bsiaw/settings.py
+++ bsiaw/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-for-local-dev'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost,0.0.0.0').split(',')
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
