a72ff401d14fdf0c3fa445ec5d389be73100a9dc
msiergiej <89871398+msiergiej@users.noreply.github.com>
2025-12-01 03:44:16 +0100

    kurwa zapomniałem zmienić DEBUG = False

--- bsiaw/settings.py
+++ bsiaw/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)w!@3h3kxv18+atpf%*r85n(b5^8*45^)sihr(^8e=82!l_*t&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
